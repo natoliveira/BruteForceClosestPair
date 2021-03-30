82344c8d675865f58f493b3f84f7c8be90584354
natoliveira <nataliadeolive@gmail.com>
2021-03-29 23:34:31 -0300

    algoritmo Brute force Closet Pair

---
+++ cmake-build-debug/CMakeFiles/untitled15.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled15.dir/main.cpp.o"
+  "untitled15"
+  "untitled15.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled15.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
